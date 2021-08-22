a390870c638a6f0350a7acfc020e9d5e51675457
Lucky Angelo Rabosa <angelorabosa55@yahoo.com.ph>
2021-08-22 12:49:35 +0800

    changed

--- src/Routes/assessmentRoutes.js
+++ src/Routes/assessmentRoutes.js
@@ -11,23 +11,24 @@ router.post('/assessment/:user_id', async(req, res) => {
     const user_id = req.params.user_id;
 
     const {
-        fullname,
+        firstName,
+        lastName,
         student_number,
         phone_number,
-        user_describe,
-        user_present_issues,
-        user_problem_issue,
-        user_received_counseling,
-        user_if_yes,
-        user_duration,
-        user_previous_counseling
+        presentIssues,
+        describe,
+        problemIssue,
+        radio,
+        when,
+        duration,
+        previousCounseling
     } = req.body;
 
     
 
-    if(user_id, fullname, student_number, phone_number, user_describe, user_present_issues, user_problem_issue, user_received_counseling, user_if_yes, user_duration, user_previous_counseling) {
+    if(user_id, firstName, lastName, student_number, phone_number, presentIssues, describe,radio) {
         try {
-            await db.promise().query(`INSERT INTO assessment_form(userId, fullname, studentNumber, phoneNumber, userdescribe, userPresentIssues, userProblemIssue, userReceivedCounseling, userIfYes, userDuration, userPreviousCounseling) VALUES('${user_id}', '${fullname}', '${student_number}', '${phone_number}', '${user_describe}', '${user_present_issues}', '${user_problem_issue}', '${user_received_counseling}', '${user_if_yes}', '${user_duration}', '${user_previous_counseling}')  `)
+            await db.promise().query(`INSERT INTO assessment_form(userId, firstName, lastName,studentNumber, phoneNumber, userdescribe, userPresentIssues, userProblemIssue, userReceivedCounseling, userIfYes, userDuration, userPreviousCounseling) VALUES('${user_id}', '${firstName}', '${lastName}','${student_number}', '${phone_number}', '${describe}', '${presentIssues}', '${problemIssue}', '${radio}', '${when}', '${duration}', '${previousCounseling}')  `)
 
             var options = {
                 'method': 'POST',
