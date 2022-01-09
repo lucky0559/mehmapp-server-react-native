9cbbe57dd1deb9841894817eeabec2432987f007
Lucky Angelo <lrabosa@isbx.com>
2022-01-09 23:49:55 +0800

    change itextmo

--- src/Routes/assessmentRoutes.js
+++ src/Routes/assessmentRoutes.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const db = require("../db/database");
 const request = require("request");
-const itexmo = require("itexmo")({ apiCode: "TR-MEHMA790279_XE2L5" });
+// const itexmo = require("itexmo")({ apiCode: "TR-MEHMA790279_XE2L5" });
 
 const router = Router();
 
@@ -29,29 +29,29 @@ router.post("/assessment/:user_id", async (req, res) => {
         `INSERT INTO assessment_form(userId, firstName, lastName, studentNumber, phoneNumber, userdescribe, userPresentIssues, userProblemIssue, userReceivedCounseling, userIfYes, userDuration, userPreviousCounseling) VALUES('${user_id}', '${firstName}', '${lastName}', '${student_number}', '${phone_number}', '${describe}', '${presentIssues}', '${problemIssue}', '${radio}', '${when}', '${duration}', '${previousCounseling}') `
       );
 
-    // const options = {
-    //   method: "POST",
-    //   url: "https://www.itexmo.com/php_api/api.php",
-    //   headers: {},
-    //   formData: {
-    //     1: "09167517273",
-    //     2: `MeHMApp-user ${student_number} with User ID ${user_id} submit a Assessment Form.\nUser Phone Number:${phone_number}`,
-    //     3: "TR-MEHMA790279_XE2L5",
-    //     passwd: "8$e@v(yn!$"
-    //   }
-    // };
+    const options = {
+      method: "POST",
+      url: "https://www.itexmo.com/php_api/api.php",
+      headers: {},
+      formData: {
+        1: "09167517273",
+        2: `MeHMApp User ${student_number} with User ID ${user_id} submit a Assessment Form.\nUser Phone Number:${phone_number}`,
+        3: "TR-MEHMA790279_XE2L5",
+        passwd: "8$e@v(yn!$"
+      }
+    };
 
-    // request(options, (error, response) => {
-    //   if (error) throw new Error(error);
-    //   console.log(response.body);
-    // });
+    request(options, (error, response) => {
+      if (error) throw new Error(error);
+      console.log(response.body);
+    });
 
-    itexmo
-      .send({
-        to: "09167517273",
-        body: `MeHMApp-user ${student_number} with User ID ${user_id} submit a Assessment Form.\nUser Phone Number:${phone_number}`
-      })
-      .then(message => console.log(message));
+    // itexmo
+    //   .send({
+    //     to: "09167517273",
+    //     body: `MeHMApp-user ${student_number} with User ID ${user_id} submit a Assessment Form.\nUser Phone Number:${phone_number}`
+    //   })
+    //   .then(message => console.log(message));
 
     return res
       .status(200)
